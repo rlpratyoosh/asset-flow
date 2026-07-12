75a0a79805a8d6a883471344b8ea67173219622e
rlpratyoosh <pratyoosh.prakash.dev@gmail.com>
2026-07-12 10:27:13 +0530

    feat: add roles field in user model

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -4,12 +4,22 @@ import (
 	"time"
 )
 
+type Role string
+
+const (
+	RoleAdmin          Role = "Admin"
+	RoleAssetManager   Role = "AssetManager"
+	RoleDepartmentHead Role = "DepartmentHead"
+	RoleEmployee       Role = "Employee"
+)
+
 type User struct {
 	ID           string `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
 	FullName     string `gorm:"not null"`
 	Username     string `gorm:"uniqueIndex;not null"`
 	PasswordHash string `gorm:"not null"`
 	ProfilePic   string
+	Role         Role   `gorm:"type:varchar(50);default:'Employee';not null"`
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
 }
